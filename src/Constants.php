c4dd025046901f13bf14ee438253f4307f675039
Jordan Hall <jordan.hall@rapidweb.biz>
2018-03-19 17:33:54 +0000

    Use Blade for plugin pages

--- src/Constants.php
+++ src/Constants.php
@@ -5,4 +5,6 @@ namespace RapidWeb\GoogleContactsForWordPress;
 abstract class Constants
 {
     const TEXT_DOMAIN = 'google-contacts-for-wordpress';
+    const VIEWS_DIR = __DIR__.'/views';
+    const VIEWS_CACHE_DIR = __DIR__.'/../cache-views';
 }
--- src/PageHandlers/Dashboard.php
+++ src/PageHandlers/Dashboard.php
@@ -2,11 +2,17 @@
 
 namespace RapidWeb\GoogleContactsForWordPress\PageHandlers;
 
+use RapidWeb\GoogleContactsForWordPress\Constants;
+use Jenssegers\Blade\Blade;
+
+
 class Dashboard extends BasePageHandler
 {
     public function get()
     {
-        echo "hello!";
+        $blade = new Blade(Constants::VIEWS_DIR, Constants::VIEWS_CACHE_DIR);
+
+        echo $blade->make('dashboard');
     }
 
     public function post()
