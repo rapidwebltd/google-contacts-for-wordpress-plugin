f6c7142b503b8074013c1302d9f0b533720a2a5b
Jordan Hall <jordan.hall@rapidweb.biz>
2018-03-19 10:35:00 +0000

    Add basic plugin structure

---
+++ google-contacts-for-wordpress.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Google Contacts for WordPress
+Description: This plugin provides integration with Google Contacts.
+Version: 1.0.0
+Author: Rapid Web Ltd
+Author URI: http://rapidweb.biz
+Text Domain: google-contacts-for-wordpress
+Domain Path: /languages
+*/
+
+require_once __DIR__.'/vendor/autoload.php';
+
+require_once __DIR__.'/src/hooks.php';
---
+++ readme.txt
@@ -0,0 +1,12 @@
+=== Plugin Name ===
+Contributors: Google Contacts for WordPress
+Tags: google contacts, contacts, google, google peopl
+Requires at least: 4.1
+Tested up to: 4.9.1
+Stable tag: 1.0.0
+License: LGPL-3.0
+License URI: https://opensource.org/licenses/LGPL-3.0
+
+== Description ==
+
+This plugin provides integration with Google Contacts.
